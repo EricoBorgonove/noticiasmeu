1ef1fa2f3e919546dcf469497a7457da1e623d96
EricoBorgonove <borgonove@gmail.com>
2022-06-09 10:38:47 -0400

    upando os primeiros arquivos

---
+++ app/Http/Controllers/NoticiaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Noticia;
+use Carbon\Carbon;
+
+class NoticiaController extends Controller
+{
+    //
+    public function index()
+{
+    $noticias = Noticia::all();
+
+    return view('noticias.index', [
+        'noticias' => $noticias
+    ]);
+}
+
+public function create()
+{
+    return view('noticias.create');
+
+}
+public function store(Request $request)
+{
+    $dados = $request->all();
+    $dados['data_publicacao'] = Carbon::createFromFormat("d/m/Y", $dados['data_publicacao'])->format("Y-m-d");
+    $request->imagem->storeAs('public', $request->imagem->getClientOriginalName());
+    $dados['imagem'] = '/storage/' . $request->imagem->getClientOriginalName();
+    Noticia::create($dados);
+    
+    return redirect()->back()->with('mensagem', 'Registro criado com sucesso!');
+}
+public function edit()
+{
+
+}
+public function update()
+{
+
+}
+public function destroy()
+{
+
+}
+
+}
